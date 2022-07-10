31d562952173704c23428012e7357c812bf48628
Icaro G. P. Salgado <icarosalgadogp@gmail.com>
2022-07-10 20:47:58 -0300

    feature: removing graphene

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
-    "graphene_django",
     "corsheaders",
 ]
 
@@ -125,11 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# Graphene
-GRAPHENE = {
-    "SCHEMA": "blog.schema.schema",
-}
 
 # Cors
 CORS_ORIGIN_ALLOW_ALL = False
